10aca487086d183eaf69fea7b6a6c52049e2940a
Zaouia Firdaous <zaouia.firdaous@gmail.com>
2025-05-15 12:15:24 +0200

    colorGenerator

---
+++ Color-Generator/index.js
@@ -0,0 +1,10 @@
+const getColor = () => {
+  const red = Math.floor(Math.random() * 255);
+  const green = Math.floor(Math.random() * 255);
+  const blue = Math.floor(Math.random() * 255);
+
+  const color = `rgb(${red}, ${green}, ${blue})`;
+  document.body.style.background = color;
+  document.body.innerHTML = `<h2>${color}</h2>`;
+};
+setInterval(getColor, 1000);
